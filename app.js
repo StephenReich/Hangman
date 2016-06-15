7b117058d89e969e02ac58eb977ac397305355e4
Stephen Reich <stephenr@ontariosystems.com>
2016-06-15 15:18:45 -0400

    added jQuery click event functionality from logic.js to app.js to get real time response. WORKS!

--- app.js
+++ app.js
@@ -7,6 +7,11 @@
     this.num = updateTries();
     this.wrong = updateErrors();
     this.run = function(){
+      $('#btnEnter').toggleClass("buttonClicked");
+      guess = getInput();
+      checkInput(guess);
+      updateWord();
+      console.log("Errors: " + errorsRemaining);
       this.word = updateWord();
       this.num = updateTries();
       this.wrong = updateErrors();
